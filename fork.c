ffb0b142656802685ce2db22d9d8881c2e55c461
jieunKimm <qorentks7777@gmail.com>
2017-09-07 10:13:04 +0900

    jieun

--- fork.c
+++ fork.c
@@ -7,21 +7,25 @@
 int main(int argc, char *argv[])
 {
 	pid_t pid;
+	int i;
 
+	for(i=0;i<10;i++){
 	pid = fork();
+
 	if (pid == -1) {
 		perror("fork error");
 		return 0;
 	}
 	else if (pid == 0) {
-		// child
-		printf("child process with pid %d\n", 
-			getpid());
+		printf("child process with pid %d\n",getpid());
+		return 0;
 	} else {
 		// parent
 		printf("my pid is %d\n", getpid());
 		wait(0);
 	}
+	}
 	return 0;
+
 }
 
