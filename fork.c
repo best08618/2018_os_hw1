114882db043806d0f429128141f555e716727b29
chaiwee <chaiweetan@gmail.com>
2018-09-19 11:24:39 +0900

    fork

--- fork.c
+++ fork.c
@@ -7,21 +7,27 @@
 int main(int argc, char *argv[])
 {
 	pid_t pid;
+	int i=0;
 
-	pid = fork();
-	if (pid == -1) {
-		perror("fork error");
-		return 0;
-	}
-	else if (pid == 0) {
+	for(i=0; i<10;i++)
+{
+		pid = fork();
+		if (pid == -1) 
+		{
+			perror("fork error");
+			return 0;
+		}
+		else if (pid == 0) {
 		// child
-		printf("child process with pid %d\n", 
-			getpid());
-	} else {
+			
+			printf("child process with pid %d\n", getpid());
+			break;
+		} else {
 		// parent
-		printf("my pid is %d\n", getpid());
-		wait(0);
-	}
+			printf("my pid is %d\n", getpid());
+			wait(0);
+		}
+}
 	return 0;
 }
 
