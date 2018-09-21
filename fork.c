b95f97d617e44bab499f393c84853ad6284c3cef
jinhwa15 <jinhwa15@assam.dankook.ac.kr>
2018-09-21 18:40:18 +0900

    read command code

--- fork.c
+++ fork.c
@@ -3,30 +3,39 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+void signal_callback_handler(int signum)
+{
+	printf("signal");
+	printf("Caught signal %d \n",signum);
+	return ;
+}
 
 int main(int argc, char *argv[])
 {
+	signal(SIGINT, signal_callback_handler);
 	pid_t pid;
 	int i=0 ;
-	do{
+	while(i< 5) {
 	pid = fork();
 	if (pid == -1) {
 		perror("fork error");
 		return 0;
 	}
 	else if (pid == 0) {
-		// child
-		i =0 ;
-		printf("%d" , i);
-		printf("child process with pid %d\n",getpid());
-//		return 0;
+		//child
+		kill(getppid(),SIGINT);
+		printf("child process with pid %d\n", getpid());
+		return 0;
 	} 
 	else {
+		//parent
 		printf("my pid is %d\n", getpid());
 		i++;
 		wait(0);
 	}
-	}while(i > 0 && i < 5 );
+	}
 	return 0;
 	
 }
