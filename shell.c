8d54530539037b281db6f66f3cf687e75281f7ee
best08618 <best08618@naver.com>
2018-09-21 19:50:47 +0900

    string tokenizing

--- shell.c
+++ shell.c
@@ -6,42 +6,68 @@
 #include <stdlib.h>
 
 char* read_command(void);
-int main(int argc, char ** argv)
-{
+char** tokenize_command(char* line);
 
+
+int main(void)
+{
+	printf("Shell from Jinhwa");
+	char buff[1024];
 	char* line;
-	char** args;
-	int status ; 
+	char** tok_com; 
 	while(1){
+		getcwd(buff,1024);
+		printf("%s" , buff);
 		printf(">");
 		line = read_command();
-		printf("line %s", line);
+		printf("line %s\n", line);
+		tok_com = tokenize_command(line);
+		printf("tok0: %s\n " ,tok_com[0]);
 		return 0 ;
-	}
- 
-
+	} 
 }
+
+char** tokenize_command(char* line)
+{
+	char** tok_command=malloc(sizeof(char**));
+	int tok_num = 0; 
+	char* token = strtok(line," ");
+	tok_command[tok_num++] = token; 
+	while(token)
+	{
+		token = strtok(NULL," ");
+		tok_command[tok_num] = token;
+		tok_num ++ ;
+	
+	}
+	return tok_command;
+}
+
 char* read_command(void)
 {
  	int bufsize =1024;
-	int position = 0;
+	int size = 0;
 	char * buffer  = malloc(sizeof(char)* bufsize);
 	int character;
-	if(!buffer){
+	/*if(!buffer){
 		printf("allocation error");
 		exit(0);
-	}
+	}*/
 	while(1)
 	{
 		character= getchar();
 		if( character == '\n'){
-			buffer[position] = '\0';
+			buffer[size] = '\0';
 			return buffer;
 		}
 		else{
-			buffer[position] = character;
+			buffer[size] = character;
 		}
-		position ++ ;
+		size ++ ;
+		if(size >= bufsize){
+			bufsize += 1024;
+			
+		}
 	}
 
 }
