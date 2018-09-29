7f980683e9441e8ff3c9b77309ed3ff4e40108a6
sconan96 <wnrdjsj5755@naver.com>
2018-09-29 16:11:04 +0900

    fork.c

--- fork.c
+++ fork.c
@@ -6,22 +6,42 @@
 
 int main(int argc, char *argv[])
 {
-	pid_t pid;
+	pid_t pid = -1;
+
+	int i;
+	pid_t * child_pids = NULL;
+
+	for(i = 0; i < 10; i++)
+	{
 
 	pid = fork();
+
 	if (pid == -1) {
 		perror("fork error");
 		return 0;
-	}
-	else if (pid == 0) {
-		// child
-		printf("child process with pid %d\n", 
-			getpid());
+	} else if (pid == 0) {
+		printf("child process with pid %d\n",getpid());
+
+		sleep(1);
+		return 0;
 	} else {
-		// parent
-		printf("my pid is %d\n", getpid());
-		wait(0);
+		if (child_pids == NULL) {
+			child_pids = (pid_t *)malloc(sizeof(pid_t)*10);
+			memset(child_pids, 0, sizeof(*child_pids));
+					}
+		printf("parent(%d): child(%d) has created\n", getpid(), pid);
+		child_pids[i] = pid;
+		}
+	}
+
+	for (i = 0 ; i < 10 ; i++) {
+		int status;
+		int wpid = -1;
+		wpid = child_pids[9-i];
+		waitpid(wpid, &status, 0);
+		printf("parent(%d): child(%d) has completed\n", getpid(), wpid);
 	}
 	return 0;
+
 }
 
